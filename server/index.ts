a94d40bef1ab63fee97e6e07bb79e16eadf6871a
Jalppatel1905 <pateljalp2005@gmail.com>
2025-03-10 10:13:59 +0530

    Initial commit

--- server/index.ts
+++ server/index.ts
@@ -6,12 +6,23 @@ import cors from 'cors'
 import { Server, LobbyRoom } from 'colyseus'
 import { monitor } from '@colyseus/monitor'
 import { RoomType } from '../types/Rooms'
+import { SkyOffice } from './rooms/SkyOffice'
 
 // import socialRoutes from "@colyseus/social/express"
 
-import { SkyOffice } from './rooms/SkyOffice'
+
 const port = Number(process.env.PORT || 2567)
 const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js on Render!')
+})
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://localhost:${port}`)
+})
 
 // app.use(cors())
 app.use(

5c4d44486e5d32d6c690eda22fd10fbedfc976b2
Jalppatel1905 <pateljalp2005@gmail.com>
2025-03-10 17:14:21 +0530

    frontend-url change

--- server/index.ts
+++ server/index.ts
@@ -79,12 +79,12 @@ const app = express()
 
 app.use(
      cors({
-      origin: ["http://localhost:5173", "https://virtual-desk.vercel.app"],
+      origin: ["http://localhost:5173", "https://virtual-desk-eta.vercel.app"],
       methods: "GET,POST,PUT,DELETE",
       credentials: true, // Allow cookies if needed
     })
    );
-   
+
 app.use(express.json())
 
 
